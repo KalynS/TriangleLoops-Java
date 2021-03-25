3ae4048f0b15518e61cda5dda13004aa4af6c76c
Kalyn Smith <kalyn@udel.edu>
2021-03-24 20:25:06 -0400

    completed triangle

--- src/main/java/com/codedifferently/TriangleLoops.java
+++ src/main/java/com/codedifferently/TriangleLoops.java
@@ -2,18 +2,47 @@ package com.codedifferently;
 
 public class TriangleLoops {
     public static String getTriangle(int numberOfRows) {
-        return null;
-    }
+        String rows = "";
+        for(int i=1; i<numberOfRows; i++){
+            rows += getRow(i) + "\n";
+        }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+        return rows;
     }
+// create a string that empty because its being returned
+    //for loop from zero to number of stars
+// add it to the string
+    //return the stars
+    public static String getRow(int numberOfStars) {
+        String widthSize = "";
+        for(int i=0; i<numberOfStars; i++){
+            widthSize += "*";
+        }
+        return widthSize;
+    }
+    // create a string
+    // make a for loop from 1 to 5 because i know i want 4
+    // use getRow that i have up there and put k in there
+    // have to add the break?
 
     public static String getSmallTriangle() {
-        return null;
-    }
+        String output = "";
+        for(int k=1; k<5; k++){
+            output += getRow(k);
+            output += "\n";
 
+        }
+        return output;
+    }
+// same as up there but use 10
     public static String getLargeTriangle() {
-        return null;
+        String output = "";
+        for(int i=1; i<10; i++){
+            output += getRow(i);
+            output += "\n";
+
+        }
+        return output;
+
     }
 }
